11e27ed7913ea9b34ac615941c87106660551700
Marcus Poehls <marcus.poehls@gmail.com>
2018-06-02 06:27:21 +0200

    direct dependency to auth in user profile

--- boost/web/user-profile/index.js
+++ boost/web/user-profile/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const Path = require('path')
 const Routes = require('./routes')
+const Authentication = require(Path.resolve(__dirname, '..', 'authentication'))
 
-function register(server, options) {
-  server.dependency(['vision'])
+async function register(server, options) {
+  await server.register(Authentication)
 
   server.route(Routes)
   server.log('info', 'Web Plugin registered: user profile')
