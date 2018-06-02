42fa777a19420e4f703c41fa89fbb298ed020975
Marcus Poehls <marcus.poehls@gmail.com>
2018-06-02 06:26:37 +0200

    refactor console logger

--- utils/logging/console-logger.js
+++ utils/logging/console-logger.js
@@ -12,8 +12,8 @@ class PinoConsole {
           // the "data" parameter (here destructured to { level, msg, time })
           // contains the actual data to log
           formatter: ({ level, msg, time }) => {
-            const Color = this.getChalkColor(level)
             const label = Pino.levels.labels[level]
+            const Color = this.getChalkColor(label)
 
             // this prints a message like: "1519227724068 info Your geeky log message"
             return `${Chalk.gray(time)} ${Color(label)} ${msg}`
@@ -24,29 +24,21 @@ class PinoConsole {
       config
     )
 
-    this.logger = new Pino(options)
-    this.colors = this.colorLevels()
-
-    return this.logger
+    return new Pino(options)
   }
 
-  // log(level, msg, ...options) {
-  //   this.logger.log(level, msg, ...options)
-  // }
-
   /**
-   * Pino uses integers (e.g., 30, 40, etc.)
-   * to represent log levels:
+   * Pino represents log levels as integers (e.g., 30, 40, etc.)
    *
    * trace = 10 (grey)
    * debug = 20 (blue)
    * info  = 30 (green)
    * warn  = 40 (yellow)
-   * error = 50 (red )
+   * error = 50 (red)
    * fatal = 60 (bold red)
    *
-   * Return a chalk function for the
-   * related log level, e.g.:
+   * Return a chalk function for the related log level,
+   * to print colored logs. E.g.,
    * info  => green
    * warn  => yellow
    * error => red
@@ -54,30 +46,26 @@ class PinoConsole {
    * @param {integer} level - Pino log level in numbered format
    */
   getChalkColor(level) {
-    // calculate the array position based on the Pino log level
-    const position = level / 10 - 1
-
-    // now grab the Chalk function like it's hot
-    return this.color(position) || Chalk.white
+    return this.color(level) || Chalk.white
   }
 
-  color(color) {
-    return this.colors[color]
+  color(level) {
+    return this.levelColors()[level]
   }
 
   /**
    * Color levels, ranked ascending
    * from chilly to freakout
    */
-  colorLevels() {
-    return [
-      Chalk.grey,
-      Chalk.blue,
-      Chalk.green,
-      Chalk.yellow,
-      Chalk.red,
-      Chalk.bold.red // this second bold red is for "fatal" errors
-    ]
+  levelColors() {
+    return {
+      trace: Chalk.grey,
+      debug: Chalk.blue,
+      info: Chalk.green,
+      warn: Chalk.yellow,
+      error: Chalk.red,
+      fatal: Chalk.bold.red
+    }
   }
 }
 
