2dc976d7c6b13003f2a7c96462401ca2cea85b03
Marcus Poehls <marcus.poehls@gmail.com>
2018-07-17 13:10:36 +0200

    update globals

--- bootstrap/globals.js
+++ bootstrap/globals.js
@@ -2,15 +2,26 @@
 
 const Path = require('path')
 
-// simplify imports with a global function that
-// imports local modules starting from project's root path
-// ! NO   require(Path.resolve('../../../utils/logger'))
-// ? OK   require(Path.resolve(__appRoot, 'utils', 'logger')
-// ? OK   require(`${__appRoot}/utils/logger`)
-// * YES  frequire('utils', 'logger')
-// * YES  frequire('utils/logger')
 global.__appRoot = Path.resolve(__dirname, '..')
+global.__storagePath = Path.resolve(__appRoot, 'storage')
+
+/**
+ * Simplify imports with a global function that
+ * imports local modules starting from project's
+ * root path.
+ *
+ * @example
+ * ```js
+ * const { User } = frequire('app/models')
+ * const { Logger } = frequire('boost/utils/logger')
+ * ```
+ */
 global.frequire = (...path) => require(Path.resolve(__appRoot, ...path))
 
-global.model = (...path) => require(Path.resolve(__appRoot, 'app', 'models', ...path))
-global.util = (...path) => require(Path.resolve(__appRoot, 'boost', 'utils', ...path))
+/**
+ * Shorthand functions to quickly import
+ * utilities.
+ *
+ * @param {*} path
+ */
+global.util = (...path) => frequire('boost', 'utils', ...path)
