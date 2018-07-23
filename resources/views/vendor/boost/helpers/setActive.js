186909621f4d8fb4bde661538bb3f69f1d678f5b
Marcus Poehls <marcus.poehls@gmail.com>
2018-07-23 16:27:11 +0200

    add setActive handlebars helper

---
+++ resources/views/vendor/boost/helpers/setActive.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * Returns the string "active"  if the `urlPath`
+ * matches the current request path. Returns an
+ * empty string if the values don't match.
+ *
+ * @returns {String}
+ */
+function setActive(urlPath, context) {
+  return context.data.root.request.path === urlPath ? 'active' : ''
+}
+
+module.exports = setActive
