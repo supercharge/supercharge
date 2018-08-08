bc16881055232f6f8fcaeb6b9e7e6832e788bc36
Marcus Poehls <marcus.poehls@gmail.com>
2018-08-08 04:54:32 +0200

    update mongoose connection

--- app/models/index.js
+++ app/models/index.js
@@ -1,34 +1,27 @@
 'use strict'
 
-const Mongoose = require('mongoose')
+const Config = util('config')
 const User = require('./user')
+const Mongoose = require('mongoose')
 
-// tell Mongoose to use Node.js promises
 Mongoose.Promise = global.Promise
 
-// Connect to your database
+Mongoose.connection.on('error', err => {
+  console.error(`⚡️ 🚨 ⚡️ 🚨 ⚡️ 🚨  → ${err.message}`)
+})
+
 Mongoose.connect(
   process.env.DATABASE,
   {
-    // see http://mongoosejs.com/docs/guide.html#indexes
-    // Mongoose tries to create indexes on startup
-    // while nice for development, it is recommended this
-    // behavior be disabled in production since index
+    useNewUrlParser: true,
+    // Mongoose tries to create indexes on startup.
+    // While nice for development, it’s recommended
+    // to disable it in production since index
     // creation can cause a significant performance impact
-    // -> auto index only outside production
-    autoIndex: process.env.NODE_ENV !== 'production',
-    useNewUrlParser: true
+    autoIndex: Config.get('app.env') !== 'production'
   }
 )
 
-// listen for connection errors and print the message
-Mongoose.connection.on('error', err => {
-  console.error(`⚡️ 🚨 ⚡️ 🚨 ⚡️ 🚨 ⚡️ 🚨 ⚡️ 🚨  → ${err.message}`)
-})
-
-// use ES6 shorthands: "propertyName: variableName" equals "propertyName"
 module.exports = {
-  User,
-  Test: User,
-  Make: User
+  User
 }
