e487f99f4a14e2966f121220b23986ecee7fe525
Marcus Poehls <marcus.poehls@gmail.com>
2018-08-21 16:59:09 +0200

    add default database connection in config

--- config/database.js
+++ config/database.js
@@ -5,6 +5,16 @@ const Env = util('env')
 module.exports = {
   /**
    * --------------------------------------------------------------------------
+   * Default Database Connection
+   * --------------------------------------------------------------------------
+   *
+   * tba.
+   *
+   */
+  default: Env.get('DB_CONNECTION', 'mongodb'),
+
+  /**
+   * --------------------------------------------------------------------------
    * MongoDB Connection Details
    * --------------------------------------------------------------------------
    *
@@ -17,9 +27,11 @@ module.exports = {
     host: Env.get('MONGODB_HOST', 'localhost'),
     port: Env.get('MONGODB_PORT', 27017),
     database: Env.get('MONGODB_DATABASE', 'boost'),
-    username: Env.get('MONGODB_USERNAME'),
-    password: Env.get('MONGODB_PASSWORD'),
     options: {
+      // auth: {
+      //   user: Env.get('MONGODB_USERNAME'),
+      //   password: Env.get('MONGODB_PASSWORD')
+      // },
       useNewUrlParser: true
     }
   }
