3fc121770cfba8eb876cf53ba6e6ab75dae16a85
Marcus Poehls <marcus.poehls@gmail.com>
2018-09-06 22:48:30 +0200

    add comments to test concerns

--- boost/utils/testing/concerns/creates-fake-data.js
+++ boost/utils/testing/concerns/creates-fake-data.js
@@ -3,7 +3,16 @@
 const User = model('user')
 const Uuid = require('uuid/v4')
 
+/**
+ * This class provides methods to conveniently
+ * provide fake data in the class methods,
+ * like fake users.
+ */
 class CreatesFakeData {
+  /**
+   * Create a fake user instance without
+   * saving it to the database.
+   */
   fakeUser() {
     return new User({ email: `user-${Uuid()}@mail.com` })
   }
--- boost/utils/testing/concerns/registers-hooks.js
+++ boost/utils/testing/concerns/registers-hooks.js
@@ -2,15 +2,40 @@
 
 const Ava = require('ava')
 
+/**
+ * Register empty hooks as default
+ * for each new test class.
+ */
 class RegistersHooks {
-  before() {}
+  /**
+   * Run actions before all test cases
+   * in the test class. This method
+   * runs before `beforeEach`.
+   */
+  async before() {}
 
-  beforeEach() {}
+  /**
+   * Run actions before each test case
+   * in the test class.
+   */
+  async beforeEach() {}
 
-  after() {}
+  /**
+   * Run actions after all test cases
+   * in the test class. This method
+   * runs after `afterEach`.
+   */
+  async after() {}
 
-  afterEach() {}
+  /**
+   * Run actions after each test case
+   * in the test class.
+   */
+  async afterEach() {}
 
+  /**
+   * Register hooks to the test runner.
+   */
   assignHooks() {
     this.assignBeforeHook()
     this.assignBeforeEachHook()
@@ -18,22 +43,36 @@ class RegistersHooks {
     this.assignAfterEachHook()
   }
 
+  /**
+   * Register the `before` hook.
+   */
   assignBeforeHook() {
     Ava.before(`${this.constructor.name}: before`, async t => {
       return this.before(t)
     })
   }
 
+  /**
+   * Register the `beforeEach` hook.
+   */
   assignBeforeEachHook() {
-    Ava.beforeEach(`${this.constructor.name}: beforeEach`, async t => this.beforeEach(t))
+    Ava.beforeEach(`${this.constructor.name}: beforeEach`, async t => {
+      return this.beforeEach(t)
+    })
   }
 
+  /**
+   * Register the `after` hook.
+   */
   assignAfterHook() {
     Ava.after(`${this.constructor.name}: after`, async t => {
       return this.after(t)
     })
   }
 
+  /**
+   * Register the `afterEach` hook.
+   */
   assignAfterEachHook() {
     Ava.afterEach(`${this.constructor.name}: afterEach`, async t => {
       return this.afterEach(t)
