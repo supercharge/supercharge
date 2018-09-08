9085e99a7fe0dddecf75323a598f63b8fbb5bc36
Marcus Poehls <marcus.poehls@gmail.com>
2018-09-08 10:29:27 +0200

    use extends-classes package

--- boost/utils/testing/concerns.js
+++ boost/utils/testing/concerns.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Many = require('./extend-classes')
+const Many = require('extends-classes')
 const Http = require('./concerns/makes-http-requests')
 const FakeData = require('./concerns/creates-fake-data')
 const RegistersHook = require('./concerns/registers-hooks')
@@ -10,6 +10,16 @@ const RegistersHook = require('./concerns/registers-hooks')
  * test concerns into a single class
  * which extends the base test.
  */
-class Concerns extends Many(Http, FakeData, RegistersHook) {}
+class Concerns extends Many(Http, FakeData, RegistersHook) {
+  /**
+   * Prints out the method name that
+   * is unavailable on a class.
+   *
+   * @param {String} method
+   */
+  __call(method) {
+    console.log(`'${method}()' is missing!`)
+  }
+}
 
 module.exports = Concerns
