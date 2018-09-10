ef0659d70fe551c146b4bcd4e6a7db4277093008
Marcus Poehls <marcus.poehls@gmail.com>
2018-09-10 16:51:04 +0200

    comment pending request class

--- boost/utils/testing/pending-request.js
+++ boost/utils/testing/pending-request.js
@@ -5,7 +5,15 @@ const Path = require('path')
 const Cookie = require('cookie')
 const Launch = require(Path.resolve(__appRoot, 'start'))
 
+/**
+ * A helper class to create HTTP requests in
+ * tests. This class simplifies HTTP testing
+ * as a wrapper around the hapi server.
+ */
 class PendingRequest {
+  /**
+   * Create a new instance.
+   */
   constructor () {
     this.user = null
     this.server = null
@@ -14,6 +22,10 @@ class PendingRequest {
     this.payload = {}
   }
 
+  /**
+   * Create a new hapi server instance
+   * for the HTTP test.
+   */
   async createServer () {
     const launch = new Launch()
     const server = Launch.createHapiServer()
@@ -22,24 +34,47 @@ class PendingRequest {
     return server
   }
 
+  /**
+   * Add a request header.
+   *
+   * @param {String} name
+   * @param {String} value
+   */
   header (name, value) {
     this.headers[name] = value
 
     return this
   }
 
+  /**
+   * Add an object of request headers.
+   *
+   * @param {Object} headers
+   */
   headers (headers) {
     Object.assign(this.headers, headers)
 
     return this
   }
 
+  /**
+   * Add a request cookie.
+   *
+   * @param {String} name
+   * @param {String} value
+   */
   cookie (name, value) {
     this.cookies[name] = value
 
     return this
   }
 
+  /**
+   * Create a hapi server without the
+   * named middleware.
+   *
+   * @param {Array<String>} names
+   */
   withoutMiddleware (names) {
     names = Array.isArray(names) ? names : [names]
 
@@ -48,18 +83,34 @@ class PendingRequest {
     return this
   }
 
+  /**
+   * Send user credentials to authenticate
+   * the request.
+   *
+   * @param {Object} user
+   */
   actAs (user) {
     this.user = user
 
     return this
   }
 
+  /**
+   * Sent a GET request to the given URI.
+   *
+   * @param {Object} arguments
+   */
   get ({ uri, headers }) {
     this.headers(headers)
 
     return this.inject({ method: 'GET', uri })
   }
 
+  /**
+   * Sent a POST request to the given URI.
+   *
+   * @param {Object} arguments
+   */
   post ({ uri, headers, payload }) {
     this.headers(headers)
     this.payload = payload
@@ -67,6 +118,11 @@ class PendingRequest {
     return this.inject({ method: 'POST', uri })
   }
 
+  /**
+   * Sent a PUT request to the given URI.
+   *
+   * @param {Object} arguments
+   */
   put ({ uri, headers, payload }) {
     this.headers(headers)
     this.payload = payload
@@ -74,6 +130,11 @@ class PendingRequest {
     return this.inject({ method: 'PUT', uri })
   }
 
+  /**
+   * Sent a PATCH request to the given URI.
+   *
+   * @param {Object} arguments
+   */
   patch ({ uri, headers, payload }) {
     this.headers(headers)
     this.payload = payload
@@ -81,6 +142,11 @@ class PendingRequest {
     return this.inject({ method: 'PATCH', uri })
   }
 
+  /**
+   * Sent a DELETE request to the given URI.
+   *
+   * @param {Object} arguments
+   */
   delete ({ uri, headers, payload }) {
     this.headers = headers
     this.payload = payload
@@ -88,6 +154,12 @@ class PendingRequest {
     return this.inject({ method: 'DELETE', uri })
   }
 
+  /**
+   * Inject the request and return
+   * the response.
+   *
+   * @param {arguments} arguments
+   */
   async inject ({ method, url }) {
     this.server = await this.createServer()
 
@@ -103,6 +175,11 @@ class PendingRequest {
     })
   }
 
+  /**
+   * Format the cookies to an array of strings
+   * that will joined and sent as the cookie
+   * value with the request.
+   */
   formatCookies () {
     return _.map(this.cookies, (value, name) => {
       return Cookie.serialize(name, value)
