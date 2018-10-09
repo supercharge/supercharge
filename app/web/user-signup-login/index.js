eaef6f66f59c9ac946285ae56a301630636705ce
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-09 17:17:53 +0200

    update plugin logs

--- app/web/user-signup-login/index.js
+++ app/web/user-signup-login/index.js
@@ -4,7 +4,7 @@ const Routes = require('./routes')
 
 function register (server) {
   server.route(Routes)
-  server.log('info', 'Plugin registered: user signup, login, password reset')
+  server.log('info', 'Plugin registered: user signup and login')
 }
 
 exports.plugin = {
