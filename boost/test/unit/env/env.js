ab4876435f4e2a3d32898383b5e28d8df5b8fdc8
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-11 13:36:01 +0200

    update env tests

--- boost/test/unit/env/env.js
+++ boost/test/unit/env/env.js
@@ -5,23 +5,9 @@ const Path = require('path')
 const BaseTest = util('base-test')
 
 class EnvTest extends BaseTest {
-  async setEnvValue (t) {
-    Env.set('BOOST_TEMP', 'temp-value')
-    const value = Env.get('BOOST_TEMP')
-
-    t.is(value, 'temp-value')
-    delete process.env.BOOST_TEMP
-  }
-
-  async getEnvValueFallback (t) {
-    const value = Env.get('NOT_AVAILABLE', 'tmp')
-    t.is(value, 'tmp')
-  }
-
   async usesEnvPath (t) {
     process.env.ENV_PATH = Path.resolve(__dirname, 'secrets.env')
     const envFile = Env.envFileName()
-
     t.is(envFile, process.env.ENV_PATH)
     delete process.env.ENV_PATH
   }
@@ -29,20 +15,35 @@ class EnvTest extends BaseTest {
   async loadsEnvFileFromNodeEnv (t) {
     process.env.NODE_ENV = 'temp'
     const envFile = Env.envFileName()
-
     t.is(envFile, '.env.temp')
   }
 
   async loadsEnvFileWithoutNodeEnv (t) {
     delete process.env.NODE_ENV
     const envFile = Env.envFileName()
-
     t.is(envFile, '.env')
   }
+
   async loadsCustomEnvFile (t) {
     Env.load(Path.resolve(__dirname, 'secrets.env'))
-
     t.is(Env.get('VALUE'), 'Boost')
+  }
+
+  async setEnvValue (t) {
+    Env.set('BOOST_TEMP', 'temp-value')
+    const value = Env.get('BOOST_TEMP')
+    t.is(value, 'temp-value')
+    delete process.env.BOOST_TEMP
+  }
+
+  async returnsUndefined (t) {
+    const value = Env.get('NOT_AVAILABLE')
+    t.is(value, undefined)
+  }
+
+  async returnsFallbackFallback (t) {
+    const value = Env.get('NOT_AVAILABLE', 'tmp')
+    t.is(value, 'tmp')
   }
 }
 
