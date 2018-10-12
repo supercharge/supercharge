c33b302467be753e9d37d99f99b615bf97a5784c
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-12 10:38:31 +0200

    minor refactoring to isEqual helper

--- resources/views/helpers/isEqual.js
+++ resources/views/helpers/isEqual.js
@@ -1,15 +1,18 @@
 'use strict'
 
-function isEqual(first, second, options) {
+/**
+ * Check whether two arguments are equal.
+ *
+ * @param {Mixed} first
+ * @param {Mixed} second
+ * @param {Object} options
+ */
+function isEqual (first, second, options) {
   if (arguments.length < 3) {
-    throw new Error('Handlebars Helper "isEqual" needs 2 parameters')
+    throw new Error('Handlebars helper "isEqual" needs 2 parameters')
   }
 
-  if (first === second) {
-    return options.fn(this)
-  }
-
-  return options.inverse(this)
+  return first === second ? options.fn(this) : options.inverse(this)
 }
 
 module.exports = isEqual
