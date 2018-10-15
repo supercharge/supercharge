5796c75ee12ee8eb404c8dc836e5b738e6f20099
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-15 18:41:48 +0200

    add empty console logger test file

---
+++ boost/test/unit/logger/console-logger.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const BaseTest = util('base-test')
+const ConsoleLogger = util('logging/console-logger')
+
+class ConsoleLoggerTest extends BaseTest {
+}
+
+module.exports = new ConsoleLoggerTest()
