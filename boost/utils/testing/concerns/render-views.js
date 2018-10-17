b622800367f0afa90228f03848c9c75a42674b44
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-17 16:22:25 +0200

    use view compiler util

--- boost/utils/testing/concerns/render-views.js
+++ boost/utils/testing/concerns/render-views.js
@@ -1,36 +1,14 @@
 'use strict'
 
-const Launch = frequire('start')
+const ViewCompiler = util('view/compiler')
 
 class RenderViews {
   constructor () {
-    this.compiler = null
+    this.compiler = new ViewCompiler()
   }
 
-  async init () {
-    const launch = new Launch()
-    await launch.warmUpCore()
-    await launch.configureViews()
-
-    this.compiler = launch.server.realm.plugins.vision.manager._engines.hbs.module
-  }
-
-  async compile (template) {
-    if (!this.compiler) {
-      await this.init()
-    }
-
-    return this.compiler.compile(template)
-  }
-
-  async render (template, data) {
-    if (!this.compiler) {
-      await this.init()
-    }
-
-    const renderFunction = await this.compile(template)
-
-    return renderFunction(data)
+  async render (template, data = {}) {
+    return this.compiler.render(template, data)
   }
 }
 
