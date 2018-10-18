a3782bcc557deac306d7dbdb0125b73bae58ccad
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-18 10:36:44 +0200

    update mongoose connector

--- boost/utils/database/mongoose-connector.js
+++ boost/utils/database/mongoose-connector.js
@@ -17,37 +17,20 @@ class MongooseConnector {
    *
    * @param {Object} config
    */
-  constructor (config) {
-    if (!config) {
+  constructor (connectionConfig) {
+    if (!connectionConfig) {
       throw new Error('Mongoose connector config missing. Define the mongoose connection settings in your config/database.js file.')
     }
 
-    this.config = config
-
-    // to avoid "ensureIndex" deprecation warning
-    Mongoose.set('useCreateIndex', true)
-
-    Mongoose.Promise = global.Promise
-    Mongoose.connection.on('error', this.onConnectionError)
+    this.connectionConfig = connectionConfig
+    this.configureMongoose()
   }
 
   /**
    * Create the MongoDB connection.
    */
   async connect () {
-    await Mongoose.connect(
-      this.connectionString(),
-      {
-        /**
-         * Mongoose tries to create indexes on startup.
-         * While nice for development, it’s recommended
-         * to disable it in production since index
-         * creation can cause a significant performance impact
-         */
-        autoIndex: Config.get('app.env') !== 'production',
-        ...this.config.options
-      }
-    )
+    await Mongoose.connect(this.connectionString(), this.connectionConfig.options)
   }
 
   /**
@@ -70,10 +53,47 @@ class MongooseConnector {
    * the database configuration.
    */
   connectionString () {
-    const { host, port, database } = this.config
+    const { host, port, database } = this.connectionConfig
     return `mongodb://${host}:${port}/${database}`
   }
 
+  /**
+   * Configure the global Mongoose instance
+   * based on the user configuration and
+   * add connection listeners.
+   */
+  configureMongoose () {
+    const config = Config.get('database.mongoose')
+
+    this.setMongooseOptions(config)
+    this.createConnectionListeners()
+  }
+
+  /**
+   * Customize the global Mongoose instance.
+   *
+   * @param {Object} config
+   */
+  setMongooseOptions (config) {
+    const options = config.set
+
+    Object.keys(options).forEach(key => {
+      Mongoose.set(key.toString(), options[key])
+    })
+  }
+
+  /**
+   * Create listeners for connection events.
+   */
+  createConnectionListeners () {
+    Mongoose.connection.on('error', this.onConnectionError)
+  }
+
+  /**
+   * Handle connection errors.
+   *
+   * @param {Object} err
+   */
   onConnectionError (err) {
     Logger.error(`⚡️ 🚨 Mongoose Error → ${err.message}`)
   }
