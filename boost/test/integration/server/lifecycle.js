c1709e33f2cea0c18133e267a9465f1e83cf9db1
Marcus Poehls <marcus.poehls@gmail.com>
2018-10-19 15:48:42 +0200

    add server lifecycle tests

---
+++ boost/test/integration/server/lifecycle.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const Path = require('path')
+const BaseTest = util('base-test')
+const Launch = require(Path.resolve(__appRoot, 'start'))
+
+class BaseRoutesTest extends BaseTest {
+  before () {
+    this.stub(console, 'error')
+    this.stub(process, 'exit')
+  }
+
+  after () {
+    console.error.restore()
+    process.exit.restore()
+  }
+
+  async serialStartsAndStopsDefaultServer (t) {
+    const launch = new Launch()
+    await launch.launchWithFullSpeed()
+
+    const server = launch.getServer()
+    t.not(server.info.started, 0)
+
+    process.emit('SIGTERM')
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+    t.is(server.info.started, 0)
+  }
+
+  async serialFailsToStartServer (t) {
+    const launch = new Launch()
+    const server = launch.getServer()
+    const stub = this.stub(server, 'start').throws(new Error())
+
+    await launch.launch()
+
+    this.sinon().assert.called(console.error)
+    this.sinon().assert.called(process.exit)
+    stub.restore()
+
+    t.pass()
+  }
+
+  async serialRunsShutdownFunctions (t) {
+    const launch = new Launch()
+    const server = launch.getServer()
+    const stub = this.stub(server, 'start').throws(new Error())
+
+    await launch.launch()
+
+    this.sinon().assert.called(console.error)
+    this.sinon().assert.called(process.exit)
+    stub.restore()
+
+    t.pass()
+  }
+}
+
+module.exports = new BaseRoutesTest()
