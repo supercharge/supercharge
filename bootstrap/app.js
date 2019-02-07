440a7af01904fa4a0ac4240d220bebc3eaee0f30
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-07 15:38:14 +0100

    remove auth strategy

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const User = require('../app/models/user')
 const Config = require('@supercharge/framework/config')
 
 /**
@@ -16,30 +15,6 @@ async function extendApp (server) {
   //     }
   //   }
   // ])
-
-  server.auth.strategy('web', 'cookie', {
-    cookie: Config.get('session.cookie'),
-    password: Config.get('app.key'),
-    isSecure: Config.get('app.isProduction'),
-    redirectTo: '/login',
-    appendNext: true,
-    ttl: Config.get('session.lifetime'),
-    validateFunc: async (_, session) => {
-      const userId = session.id
-
-      if (!userId) {
-        return { credentials: null, valid: false }
-      }
-
-      const user = await User.findById(userId)
-
-      if (!user) {
-        return { credentials: null, valid: false }
-      }
-
-      return { credentials: user, valid: true }
-    }
-  })
 }
 
 module.exports = extendApp
