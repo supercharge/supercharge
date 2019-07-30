b91b28aa0d9309d00103b3c415ceed1905801fd6
Marcus Poehls <marcus.poehls@gmail.com>
2019-07-30 06:47:06 +0200

    add CSRF middleware

---
+++ app/middleware/verify-csrf-token.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const Extension = require('@supercharge/framework/http/middleware/verify-csrf-token')
+
+class VerifyCsrfToken extends Extension {}
+
+module.exports = VerifyCsrfToken
