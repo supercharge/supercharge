90ae128af348dc233cd9db8ce56eb21f15243bed
Marcus Pöhls <marcus.poehls@gmail.com>
2019-08-05 18:08:33 +0200

    add faktory queue config

--- config/queue.js
+++ config/queue.js
@@ -11,6 +11,23 @@ module.exports = {
    * tba.
    *
    */
-  driver: Env.get('QUEUE_DRIVER', 'sync')
+  driver: Env.get('QUEUE_DRIVER', 'sync'),
 
+  /**
+   * --------------------------------------------------------------------------
+   * Queue Connections
+   * --------------------------------------------------------------------------
+   *
+   * tba.
+   *
+   */
+  connections: {
+    faktory: {
+      url: Env.get('FAKTORY_QUEUE_URL'),
+      host: Env.get('FAKTORY_QUEUE_HOST'),
+      port: Env.get('FAKTORY_QUEUE_PORT'),
+      password: Env.get('FAKTORY_QUEUE_PASSWORD'),
+      queue: Env.get('FAKTORY_QUEUE_NAME', 'default')
+    }
+  }
 }
