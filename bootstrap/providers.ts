69ffba3482cade033b1eb2ebfad7b8222eb17fe2
Marcus Pöhls <marcus.poehls@gmail.com>
2020-12-09 10:10:56 +0100

    route service provider

--- bootstrap/providers.ts
+++ bootstrap/providers.ts
@@ -1,11 +1,15 @@
 'use strict'
 
+import { RouteServiceProvider } from '../app/providers/route-service-provider'
+
 export const providers: any[] = [
   /**
    * All listed providers will be registered and booted while starting
    * your application. Go ahead and add your own providers to this
    * list to register custom functionality to your application.
    */
+  RouteServiceProvider,
+
   // require('@supercharge/hashing'),
   // require('@supercharge/framework/session/bootstrapper'),
 ]
