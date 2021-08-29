27178e5bde458367c2d80cd70768def9bcf879f3
Marcus Pöhls <mp@seda.digital>
2021-08-29 13:21:17 +0200

    add bodyparser options

--- config/bodyparser.ts
+++ config/bodyparser.ts
@@ -26,19 +26,78 @@ export default {
    *
    * see http://tools.ietf.org/html/draft-ietf-httpbis-p2-semantics-19#section-6.3
    */
-   methods: ['POST', 'PUT', 'PATCH'],
+  methods: ['POST', 'PUT', 'PATCH'],
 
   /**
    * --------------------------------------------------------------------------
    * JSON Parser Options
    * --------------------------------------------------------------------------
    *
-   * {String|Integer} The byte (if integer) limit of the JSON body, default 1mb
+   * This section configures the JSON parsing options. The given options only
+   * apply to JSON parsing and you may adjust the settings to your needs.
    *
    */
   json: {
+    /**
+     * ------------------------------------------------------------------------
+     * JSON Parsing Limit
+     * ------------------------------------------------------------------------
+     *
+     * This option configures the JSON body limit. The body parser throws an
+     * HTTP error with status code 413 if an incoming request body exceeds
+     * the given limit.
+     */
     limit: '1mb',
-    strict: true,
+
+    /**
+     * ------------------------------------------------------------------------
+     * JSON Parsing Content Types
+     * ------------------------------------------------------------------------
+     *
+     * This option defines the JSON content types which will be parsed. You
+     * may use the `*` wildcard symbol to allow content types confirming
+     * to the given definition.
+     */
+    contentTypes: [
+      'application/json',
+      'application/*+json',
+      'application/csp-report',
+    ],
+  },
+
+  /**
+   * --------------------------------------------------------------------------
+   * Form Parser Options
+   * --------------------------------------------------------------------------
+   *
+   * This section configures the form parsing options. The given options only
+   * apply to JSON parsing and you may adjust the settings to your needs.
+   *
+   */
+  form: {
+    /**
+     * ------------------------------------------------------------------------
+     * Form Parsing Limit
+     * ------------------------------------------------------------------------
+     *
+     * This option configures the JSON body limit. The body parser throws an
+     * HTTP error with status code 413 if an incoming request body exceeds
+     * the given limit.
+     */
+    limit: '56kb',
+
+    /**
+     * ------------------------------------------------------------------------
+     * Form Parsing Content Types
+     * ------------------------------------------------------------------------
+     *
+     * This option defines the form content types which will be parsed. You
+     * may use the `*` wildcard symbol to allow content types confirming
+     * to the given definition.
+     */
+     contentTypes: [
+      'application/x-www-form-urlencoded'
+     ]
   },
 
   /**
@@ -46,23 +105,77 @@ export default {
    * Text Parser Options
    * --------------------------------------------------------------------------
    *
-   * {String|Integer} The byte (if integer) limit of the JSON body, default 1mb
+   * This section configures the text parsing options. The given options only
+   * apply to text parsing and you may adjust the settings to your needs.
    *
    */
-  text: {
+   text: {
+    /**
+     * ------------------------------------------------------------------------
+     * Text Parsing Limit
+     * ------------------------------------------------------------------------
+     *
+     * This option configures the text body limit. The body parser throws an
+     * HTTP error with status code 413 if an incoming request body exceeds
+     * the given limit.
+     */
     limit: '56kb',
+
+    /**
+     * ------------------------------------------------------------------------
+     * Text Parsing Content Types
+     * ------------------------------------------------------------------------
+     *
+     * This option defines the JSON content types which will be parsed. You
+     * may use the `*` wildcard symbol to allow content types confirming
+     * to the given definition.
+     */
+    contentTypes: ['text/*'],
   },
 
 
   /**
    * --------------------------------------------------------------------------
-   * Form Parser Options
+   * Multipart Parser Options
    * --------------------------------------------------------------------------
    *
-   * {String|Integer} The byte (if integer) limit of the JSON body, default 1mb
+   * This section configures the multipart parsing options. The given options
+   * apply to multipart parsing and you may adjust the settings to your needs.
    *
    */
-  formUrlEncoded: {
-    limit: '56kb',
-  },
+   multipart: {
+    /**
+     * ------------------------------------------------------------------------
+     * Multipart File Size Limit
+     * ------------------------------------------------------------------------
+     *
+     * This option configures the multipart body limit. The body parser throws
+     * an HTTP error with status code 413 if an incoming request body exceeds
+     * the given limit.
+     */
+    limit: '20mb',
+
+    /**
+     * ------------------------------------------------------------------------
+     * Multipart Parsing Content Types
+     * ------------------------------------------------------------------------
+     *
+     * This option defines the multipart content types which will be parsed. You
+     * may use the `*` wildcard symbol to allow content types confirming
+     * to the given definition.
+     */
+    contentTypes: [
+      'multipart/form-data'
+    ],
+
+    /**
+     * ------------------------------------------------------------------------
+     * Maximum Number of Multipart Fields
+     * ------------------------------------------------------------------------
+     *
+     * This option defines the maximum number of fields allowed in the request
+     * body. **Notice:** setting this option to `0` means unlimited fields.
+     */
+    maxFields: 1000
+  }
 }
